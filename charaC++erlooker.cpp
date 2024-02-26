7948b56123e8ef9d55dca8741e02ab04ac146205
Lights-Feel-Beautiful <115139994+Jeanroe@users.noreply.github.com>
2024-02-26 21:54:16 +0300

    Add files via upload

---
+++ charaC++erlooker.cpp
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html>
+<head>
+     <metacharset = "utf-8">
+	 <meta name = "viewport"
+</head>
+--This is the key to mYhome--
+--WE plamn on making it C++--
+<!--TThe purpose of this page is to act as the link to the home server and to display its content on authorisation as well as send back the data on where the user of link is from and then forming that into a geo-graphical identifier and using that to get a real address of the person-
+
+MAke sure that al of this is only for the use of my family and look intto laws and regulation regarding data and wheter i am allowed to collect without a certificate of some sort. Love lots and make sure the website is fun and interactive-->
+<body>
+</body>
+</html>
